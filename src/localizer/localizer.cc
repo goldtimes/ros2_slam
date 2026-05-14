8bb59f173fdae96798625bcc873811f78b45460f
lihang <lihang@kilox.cn>
2026-05-14 14:27:39 +0800

    "update"

--- src/localizer/localizer.cc
+++ src/localizer/localizer.cc
@@ -1,12 +1,3 @@
-/*
- * @Author: lihang lihang@kilox.cn
- * @Date: 2025-09-08 13:41:48
- * @LastEditors: lihang lihang@kilox.cn
- * @LastEditTime: 2025-09-29 15:36:36
- * @FilePath: /fast_lvio_ws/src/open_slam/src/localizer/localizer.cc
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
- * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #include "localizer/localizer.hh"
 #include <pcl/range_image/range_image.h>
 #include <boost/filesystem.hpp>
