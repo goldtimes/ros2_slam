b8970811056c5790a77bd66555f01899fed62de8
lihang <lihang@kilox.cn>
2026-05-15 08:24:14 +0000

    "改动一些"

--- app/map_node.cc
+++ app/map_node.cc
@@ -1,12 +1,3 @@
-/*
- * @Author: lihang lihang@kilox.cn
- * @Date: 2025-08-11 17:48:59
- * @LastEditors: lihang lihang@kilox.cn
- * @LastEditTime: 2025-09-10 16:39:58
- * @FilePath: /fast_lvio_ws/src/lio_slam/app/map_node.cc
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
- * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #include <ros/ros.h>
 #include "logger.hh"
 #include "ros/ros1_manager.hh"
--- include/eigen_type.hh
+++ include/eigen_type.hh
@@ -1,3 +1,4 @@
+#pragma once
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include "sophus/se2.hpp"
